03a12895d781b5d70cf24ad4d796a07a767eb451
emmanuel victorio <emmanvictorio5@gmail.com>
2024-06-05 16:05:48 +0800

    add function

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -3,7 +3,7 @@ export default function Productlist () {
         <>
          
          <div>
-  <div>Product 01</div>
+  <div>Product 011</div>
   <div>Product 02</div>
   <div>Product 03</div>
   <div>Product 04</div>
