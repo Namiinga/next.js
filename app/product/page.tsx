d21799e66ec1261d9cefc1d9a27e9594dd78ec69
emmanuel victorio <emmanvictorio5@gmail.com>
2024-06-05 16:02:44 +0800

    first commit

---
+++ app/product/page.tsx
@@ -0,0 +1,16 @@
+export default function Productlist () {
+    return (
+        <>
+         
+         <div>
+  <div>Product 01</div>
+  <div>Product 02</div>
+  <div>Product 03</div>
+  <div>Product 04</div>
+  <div>Product 05</div>
+  <div>Product 06</div>
+</div>
+        </>
+        
+    )
+}
